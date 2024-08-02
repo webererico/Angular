a20252c7b68e37ad90bff83ed5a0a1ae02bcb5e3
Érico Rosiski Weber <ericoweber@hotmail.com>
2024-08-02 20:38:29 -0300

    componente simplificado - aula 5

---
+++ src/app/componentes/componente-simplificado/componente-simplificado.component.ts
@@ -0,0 +1,17 @@
+
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-componente-simplificado',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      Componente simplificado
+    </p>
+  `,
+  styles: ``
+})
+export class ComponenteSimplificadoComponent {
+}
